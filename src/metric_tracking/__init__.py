ffed405ccac45579470d4c89f54121a95c295ea9
Josafat-Mattias Burmeister <33292321+josafatburmeister@users.noreply.github.com>
2021-11-19 16:06:48 +0100

    Update src/metric_tracking/__init__.py
    Co-authored-by: Jasper Blum <33397387+Jasperhino@users.noreply.github.com>

--- src/metric_tracking/__init__.py
+++ src/metric_tracking/__init__.py
@@ -1,4 +1,4 @@
-""" modules to import when initializing module """
+""" Modules to import when initializing module """
 
 from .metric_tracker import MetricTracker
 from .metric_per_case_tracker import MetricPerCaseTracker
