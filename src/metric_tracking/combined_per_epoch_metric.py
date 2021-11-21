cae113c963b1f783a63b809da18ae7cc8b042479
Josafat-Mattias Burmeister <josafat-mattias.burmeister@student.hpi.de>
2021-11-21 19:36:33 +0000

    refactor metric tracking code

---
+++ src/metric_tracking/combined_per_epoch_metric.py
@@ -0,0 +1,163 @@
+"""
+Module containing a metrics class for tracking and aggregating several metrics related to multiple 3D MRT scans
+whose slices may be scattered across different batches.
+"""
+
+from typing import Dict, Iterable, Optional
+
+import torch
+import torchmetrics
+
+from .combined_per_scan_metric import CombinedPerScanMetric
+
+
+class CombinedPerEpochMetric(torchmetrics.Metric):
+    """
+    A metrics class that tracks the metrics of multiple 3D MRT scans (cases) whose slices may be scattered across
+    different batches. Different metrics can be tracked per scan, e.g. dice score and Hausdorff distance, and the
+    metrics can also be tracked for different confidence levels. If `reduction` is not `"none"`, the per-scan metrics
+    are  aggregated into global per-epoch metric values.
+
+    Args:
+        phase (string): Descriptive name of the current pipeline phase, e.g. "train", "val" or "test".
+        metrics (Iterable[str]): A list of metric names to be tracked. Available options: "dice", "sensitivity",
+            "specificity", and "hausdorff95".
+        confidence_levels (Iterable[float]): A list of confidence levels for which the metrics are to be tracked
+            separately.
+        metrics_to_aggregate (Iterable[str], optional): A list of metric names from which to calculate an aggregated
+            metric. Must be a subset of the metric names passed to the `metrics` parameter. If, for example,
+            `metrics_to_aggregate=["dice", "hausdorff95"]` and `reduction="mean"`, the mean of the dice score and the
+            Hausdorff distance is calculated and returned as the `aggregated` metric. If `reduction` is `"none"`, this
+                parameter will be ignored.
+        reduction (string, optional):  Reduction function that is to be used to aggregate the metric values of all scans
+            / cases, must be either "mean", "sum" or "none". Default: `"mean"`.
+
+    Note:
+        In this method, the `prediction` tensor is expected to be the output of the final sigmoid layer of a
+            single-class segmentation task.
+
+    Shape:
+        - Prediction: :math:`(N, height, width)`, where `N = batch size`.
+        - Target: Must have the same dimensions as the prediction.
+        - Case_ids: :math:`(N)`, where `N = batch size`.
+    """
+
+    def __init__(
+        self,
+        phase: str,
+        metrics: Iterable[str],
+        confidence_levels: Iterable[float],
+        metrics_to_aggregate: Optional[Iterable[str]] = None,
+        reduction: str = "mean",
+    ):
+        super().__init__()
+        self.phase = phase
+        self.metrics = metrics
+        self.confidence_levels = confidence_levels
+
+        if metrics_to_aggregate is not None and not set(metrics_to_aggregate).issubset(
+            set(metrics)
+        ):
+            raise ValueError(
+                "'metrics_to_aggregate must be a subset' of the metric names passed to the 'metrics' parameter."
+            )
+
+        self.metrics_to_aggregate = metrics_to_aggregate
+        self._metrics_per_case = {}
+
+        if reduction not in ["mean", "sum", "none"]:
+            raise ValueError("Invalid reduction method.")
+
+        self.reduction = reduction
+
+    # pylint: disable=arguments-differ
+    def update(
+        self,
+        prediction: torch.Tensor,
+        target: torch.Tensor,
+        case_ids: Iterable[str],
+    ) -> None:
+        """
+        Takes the prediction and target of a given batch and updates the metrics accordingly.
+
+        Args:
+            prediction (Tensor): A batch of predictions.
+            target (Tensor): A batch of targets.
+            case_ids (Iterable[string]): Case IDs of each slice in the prediction and target batches.
+        """
+
+        for idx, case_id in enumerate(case_ids):
+            if case_id not in self._metrics_per_case:
+                self._metrics_per_case[case_id] = CombinedPerScanMetric(
+                    self.phase, self.metrics, self.confidence_levels
+                )
+
+            self._metrics_per_case[case_id].update(prediction[idx], target[idx])
+
+    # pylint: disable=too-many-branches
+    def compute(self) -> Dict[str, torch.Tensor]:
+        """
+        Computes the metrics for each scan and aggregates them if `reduction` is not `"none"`.
+
+        Returns:
+            Dict[string, Tensor]: Mapping of metric names to metric values.
+                If `reduction` is `"none"`, the keys have the form `<phase>/<metric name>_<confidence_level>_<case ID>`.
+                Otherwise the keys have the form `<phase>/<reduction>_<metric name>_<confidence_level>`
+                If `"metrics_to_aggregate"` is provided and `reduction` is not `"none"`, the dictionary additionally
+                contains the keys `<phase>/<reduction>_aggregated_<confidence_level>`.
+        """
+        per_case_metrics = {}
+
+        for case_id, case_metric in self._metrics_per_case.items():
+            case_metrics = case_metric.compute()
+            for metric_name, metric_value in case_metrics.items():
+                if self.reduction == "none":
+                    per_case_metrics[
+                        f"{self.phase}/{metric_name.lstrip(f'{self.phase}/')}_{case_id}"
+                    ] = metric_value
+                else:
+                    if metric_name not in per_case_metrics:
+                        per_case_metrics[metric_name] = []
+                    per_case_metrics[metric_name].append(metric_value)
+
+        if self.reduction == "none":
+            return per_case_metrics
+
+        aggregated_metrics = {}
+
+        for metric_name, metric_value in per_case_metrics.items():
+            aggregated_metric_name = (
+                f"{self.phase}/{self.reduction}_{metric_name.lstrip(f'{self.phase}/')}"
+            )
+            if self.reduction == "mean":
+                aggregated_metrics[aggregated_metric_name] = torch.tensor(
+                    per_case_metrics[metric_name]
+                ).mean()
+            elif self.reduction == "sum":
+                aggregated_metrics[aggregated_metric_name] = torch.tensor(
+                    per_case_metrics[metric_name]
+                ).sum()
+
+        if self.metrics_to_aggregate is not None:
+            for confidence_level in self.confidence_levels:
+                metric_values = []
+                for metric_name in self.metrics_to_aggregate:
+                    metric_values.append(
+                        aggregated_metrics[
+                            f"{self.phase}/{self.reduction}_{metric_name}_{confidence_level:.1f}"
+                        ]
+                    )
+
+                aggregated_metric_name = (
+                    f"{self.phase}/{self.reduction}_aggregated_{confidence_level:.1f}"
+                )
+                if self.reduction == "mean":
+                    aggregated_metrics[aggregated_metric_name] = torch.tensor(
+                        metric_values
+                    ).mean()
+                elif self.reduction == "sum":
+                    aggregated_metrics[aggregated_metric_name] = torch.tensor(
+                        metric_values
+                    ).sum()
+
+        return aggregated_metrics
--- src/models/pytorch_u_net.py
+++ src/models/pytorch_u_net.py
@@ -1,11 +1,9 @@
 """U-Net architecture wrapped as PytorchModel"""
 
-from typing import Any, Iterable
+from typing import Iterable
 
 import torch
-import wandb
 
-from metric_tracking import MetricPerCaseTracker
 from .pytorch_model import PytorchModel
 from .u_net import UNet
 
@@ -23,51 +21,6 @@ class PytorchUNet(PytorchModel):
         super().__init__(**kwargs)
 
         self.model = UNet(in_channels=1, out_channels=1, init_features=32)
-
-        self.confidence_levels = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
-        self.confidence_level_names = [
-            "0.1",
-            "0.2",
-            "0.3",
-            "0.4",
-            "0.5",
-            "0.6",
-            "0.7",
-            "0.8",
-            "0.9",
-        ]
-
-        self.train_average_metrics = MetricPerCaseTracker(
-            metrics=["dice", "sensitivity", "specificity", "hausdorff95"],
-            metrics_to_aggregate=["dice", "hausdorff95"],
-            reduce="mean",
-            groups=self.confidence_level_names,
-            device=self.device,
-        )
-
-        self.train_metrics_per_case = MetricPerCaseTracker(
-            metrics=["dice", "sensitivity", "specificity", "hausdorff95"],
-            metrics_to_aggregate=["dice", "hausdorff95"],
-            reduce="none",
-            groups=self.confidence_level_names,
-            device=self.device,
-        )
-
-        self.val_average_metrics = MetricPerCaseTracker(
-            metrics=["dice", "sensitivity", "specificity", "hausdorff95"],
-            metrics_to_aggregate=["dice", "hausdorff95"],
-            reduce="mean",
-            groups=self.confidence_level_names,
-            device=self.device,
-        )
-
-        self.val_metrics_per_case = MetricPerCaseTracker(
-            metrics=["dice", "sensitivity", "specificity", "hausdorff95"],
-            metrics_to_aggregate=["dice", "hausdorff95"],
-            reduce="none",
-            groups=self.confidence_level_names,
-            device=self.device,
-        )
 
     # wrap model interface
     def eval(self) -> None:
@@ -126,56 +79,16 @@ class PytorchUNet(PytorchModel):
         self.train_average_metrics.to(self.device)
         self.train_metrics_per_case.to(self.device)
 
-        # pylint: disable-msg=unused-variable
         x, y, case_ids = batch
 
         probabilities = self(x)
         loss = self.loss(probabilities, y)
 
-        # ToDo: log metrics for different confidence levels
-
-        for idx, confidence_level in enumerate(self.confidence_levels):
-
-            predicted_mask = (probabilities > confidence_level).int()
-
-            self.train_average_metrics.update(
-                predicted_mask, y, case_ids, group_name=self.confidence_level_names[idx]
-            )
-            self.train_metrics_per_case.update(
-                predicted_mask, y, case_ids, group_name=self.confidence_level_names[idx]
-            )
-
-        # ToDo: compute metrics on epoch end and log them to WandB
+        for train_metric in self.get_train_metrics():
+            train_metric.update(probabilities, y, case_ids)
 
         self.log("train/loss", loss)  # log train loss via weights&biases
         return loss
-
-    def training_epoch_end(self, training_step_outputs: Any):
-        """
-        This method is called by the Pytorch Lightning framework at the end of each training epoch.
-
-        Args:
-            training_step_outputs: List of return values of all training steps of the current training epoch. 
-        """
-
-        for idx, confidence_level in enumerate(self.confidence_levels):
-            average_metrics = self.train_average_metrics.compute(
-                group_name=self.confidence_level_names[idx]
-            )
-            metrics_per_case = self.train_metrics_per_case.compute(
-                group_name=self.confidence_level_names[idx]
-            )
-
-            for metric_name, metric_value in average_metrics.items():
-                wandb.define_metric(f"train/mean_{metric_name}_{confidence_level}", summary="max", step_metric="epoch")
-                self.log(f"train/mean_{metric_name}_{confidence_level}", metric_value)
-
-            for metric, metric in metrics_per_case.items():
-                for case_id, metric_value in metric.items():
-                    self.log(
-                        f"train/case_{case_id}_{metric_name}_{confidence_level}",
-                        metric_value,
-                    )
 
     def validation_step(self, batch, batch_idx) -> None:
         """
@@ -196,40 +109,5 @@ class PytorchUNet(PytorchModel):
         loss = self.loss(probabilities, y)
         self.log("validation/loss", loss)  # log validation loss via weights&biases
 
-        for idx, confidence_level in enumerate(self.confidence_levels):
-
-            predicted_mask = (probabilities > confidence_level).int()
-
-            self.val_average_metrics.update(
-                predicted_mask, y, case_ids, group_name=self.confidence_level_names[idx]
-            )
-            self.val_metrics_per_case.update(
-                predicted_mask, y, case_ids, group_name=self.confidence_level_names[idx]
-            )
-
-    def validation_epoch_end(self, validation_step_outputs: Any):
-        """
-        This method is called by the Pytorch Lightning framework at the end of each validation epoch.
-
-        Args:
-            validation_step_outputs: List of return values of all validation steps of the current validation epoch. 
-        """
-
-        for idx, confidence_level in enumerate(self.confidence_levels):
-            average_metrics = self.val_average_metrics.compute(
-                group_name=self.confidence_level_names[idx]
-            )
-            metrics_per_case = self.val_metrics_per_case.compute(
-                group_name=self.confidence_level_names[idx]
-            )
-
-            for metric_name, metric_value in average_metrics.items():
-                wandb.define_metric(f"val/mean_{metric_name}_{confidence_level}", summary="max", step_metric="epoch")
-                self.log(f"val/mean_{metric_name}_{confidence_level}", metric_value)
-
-            for metric_name, metric in metrics_per_case.items():
-                for case_id, metric_value in metric.items():
-                    self.log(
-                        f"val/case_{case_id}_{metric_name}_{confidence_level}",
-                        metric_value,
-                    )
+        for val_metric in self.get_val_metrics():
+            val_metric.update(probabilities, y, case_ids)
