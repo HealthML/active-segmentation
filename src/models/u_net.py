25780ac15a8edd5d84015d0c1cae4850cf89aa13
Jonas Kordt <jonas.kordt@student.hpi.de>
2021-11-24 17:16:46 +0100

    Implement n layer UNet

--- src/models/u_net.py
+++ src/models/u_net.py
@@ -16,44 +16,54 @@ class UNet(nn.Module):
         out_channels: Number of output channels (should be equal to the number of classes excluding the background)
         init_features: Number of feature channels of the first U-Net block, in each down-sampling block, the number of
             feature channels is doubled.
+        layers: Number of layers (encoder and decoder blocks) in the U-Net.
     """
 
     # pylint: disable-msg=too-many-instance-attributes
 
     def __init__(
-        self, in_channels: int = 3, out_channels: int = 1, init_features: int = 32
+        self,
+        in_channels: int = 3,
+        out_channels: int = 1,
+        init_features: int = 32,
+        layers: int = 4,
     ):
 
         super().__init__()
 
+        self.layers = layers
+
         features = init_features
-        self.encoder1 = UNet._block(in_channels, features, name="enc1")
-        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.encoder2 = UNet._block(features, features * 2, name="enc2")
-        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.encoder3 = UNet._block(features * 2, features * 4, name="enc3")
-        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.encoder4 = UNet._block(features * 4, features * 8, name="enc4")
-        self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2)
 
-        self.bottleneck = UNet._block(features * 8, features * 16, name="bottleneck")
+        self.encoders = [
+            UNet._block(
+                in_channels if i == 0 else features * (2 ** (i - 1)),
+                features * (2 ** i),
+                name=f"enc{i + 1}",
+            )
+            for i in range(layers)
+        ]
+        self.pools = [nn.MaxPool2d(kernel_size=2, stride=2) for _ in range(layers)]
 
-        self.upconv4 = nn.ConvTranspose2d(
-            features * 16, features * 8, kernel_size=2, stride=2
+        self.bottleneck = UNet._block(
+            features * (2 ** (layers - 1)), features * (2 ** layers), name="bottleneck"
         )
-        self.decoder4 = UNet._block((features * 8) * 2, features * 8, name="dec4")
-        self.upconv3 = nn.ConvTranspose2d(
-            features * 8, features * 4, kernel_size=2, stride=2
-        )
-        self.decoder3 = UNet._block((features * 4) * 2, features * 4, name="dec3")
-        self.upconv2 = nn.ConvTranspose2d(
-            features * 4, features * 2, kernel_size=2, stride=2
-        )
-        self.decoder2 = UNet._block((features * 2) * 2, features * 2, name="dec2")
-        self.upconv1 = nn.ConvTranspose2d(
-            features * 2, features, kernel_size=2, stride=2
-        )
-        self.decoder1 = UNet._block(features * 2, features, name="dec1")
+
+        self.upconvs = [
+            nn.ConvTranspose2d(
+                features * 2 * (2 ** (i + 1)),
+                features * (2 ** i),
+                kernel_size=2,
+                stride=2,
+            )
+            for i in range(layers)
+        ]
+        self.decoders = [
+            UNet._block(
+                features * 2 * (2 ** (i + 1)), features * (2 ** i), name=f"dec{i + 1}"
+            )
+            for i in range(layers)
+        ]
 
         self.conv = nn.Conv2d(
             in_channels=features, out_channels=out_channels, kernel_size=1
@@ -70,26 +80,19 @@ class UNet(nn.Module):
         """
 
         x = x.float()
-        enc1 = self.encoder1(x)
-        enc2 = self.encoder2(self.pool1(enc1))
-        enc3 = self.encoder3(self.pool2(enc2))
-        enc4 = self.encoder4(self.pool3(enc3))
+        encs = []  # individually store encoding results for skip connections
+        for i in range(self.layers):
+            encs[i] = self.encoders[i](x if i == 0 else self.pools[i - 1](encs[i - 1]))
 
-        bottleneck = self.bottleneck(self.pool4(enc4))
+        bottleneck = self.bottleneck(self.pool4(encs[-1]))
 
-        dec4 = self.upconv4(bottleneck)
-        dec4 = torch.cat((dec4, enc4), dim=1)
-        dec4 = self.decoder4(dec4)
-        dec3 = self.upconv3(dec4)
-        dec3 = torch.cat((dec3, enc3), dim=1)
-        dec3 = self.decoder3(dec3)
-        dec2 = self.upconv2(dec3)
-        dec2 = torch.cat((dec2, enc2), dim=1)
-        dec2 = self.decoder2(dec2)
-        dec1 = self.upconv1(dec2)
-        dec1 = torch.cat((dec1, enc1), dim=1)
-        dec1 = self.decoder1(dec1)
-        return torch.sigmoid(self.conv(dec1))
+        dec = bottleneck
+        for i in reversed(range(self.layers)):
+            dec = self.upconvs[i](dec)
+            dec = torch.cat((dec, encs[i]), dim=1)
+            dec = self.decoders[i](dec)
+
+        return torch.sigmoid(self.conv(dec))
 
     @staticmethod
     def _block(in_channels, features, name):
