20f8f28aab155e357b41cd0c48f48dfc4736d3fd
Josafat-Mattias Burmeister <josafat-mattias.burmeister@student.hpi.de>
2021-12-13 19:53:00 +0100

    add test case for metric resetting

--- tests/test_metric_tracking/test_combined_per_image_metric.py
+++ tests/test_metric_tracking/test_combined_per_image_metric.py
@@ -15,40 +15,12 @@ class TestCombinedPerImageMetric(unittest.TestCase):
     """
 
     @staticmethod
-    def _hausdorff_distance(
-        dists_prediction_target: List[float],
-        dists_target_prediction: List[float],
-        percentile: int = 95,
-    ) -> float:
-        r"""
-        Computes expected Hausdorff distance.
-
-        Args:
-            dists_prediction_target: List that contains for each positive prediction pixel the distance to the closest
-                positive target pixel.
-            dists_target_prediction: List that contains for each positive target pixel the distance to the closest
-                positive prediction pixel
-            percentile (float, optional): Percentile for which the Hausdorff distance is to be calculated, must be in
-                :math:`\[0, 100\]`.
+    def example_data():
+        """
+        Creates a faked segmentation exmaple that contains both true and false predictions.
 
         Returns:
-            float: Hausdorff distance.
-        """
-
-        distances = np.hstack((dists_prediction_target, dists_target_prediction))
-        hausdorff_distance = torch.as_tensor(
-            np.percentile(
-                distances,
-                q=percentile,
-            )
-        ).float()
-
-        return hausdorff_distance
-
-    # pylint: disable=too-many-locals
-    def test_standard_case(self):
-        """
-        Tests that the CombinedPerImageMetric class correctly computes all metrics for all confidence levels.
+            Tuple: Predicted slice, target slice, TP, FP, TN, FN for the confidence levels 0.2, 0.5, and 0.8.
         """
 
         predicted_probabilities = torch.Tensor(
@@ -94,6 +66,63 @@ class TestCombinedPerImageMetric(unittest.TestCase):
         tp_0_2, fp_0_2, tn_0_2, fn_0_2 = 7, 7, 6, 7
         tp_0_5, fp_0_5, tn_0_5, fn_0_5 = 5, 3, 10, 9
         tp_0_8, fp_0_8, tn_0_8, fn_0_8 = 1, 0, 13, 13
+
+        return (
+            predicted_probabilities,
+            target,
+            (tp_0_2, fp_0_2, tn_0_2, fn_0_2),
+            (tp_0_5, fp_0_5, tn_0_5, fn_0_5),
+            (tp_0_8, fp_0_8, tn_0_8, fn_0_8),
+        )
+
+    @staticmethod
+    def _hausdorff_distance(
+        dists_prediction_target: List[float],
+        dists_target_prediction: List[float],
+        percentile: int = 95,
+    ) -> float:
+        r"""
+        Computes expected Hausdorff distance.
+
+        Args:
+            dists_prediction_target: List that contains for each positive prediction pixel the distance to the closest
+                positive target pixel.
+            dists_target_prediction: List that contains for each positive target pixel the distance to the closest
+                positive prediction pixel
+            percentile (float, optional): Percentile for which the Hausdorff distance is to be calculated, must be in
+                :math:`\[0, 100\]`.
+
+        Returns:
+            float: Hausdorff distance.
+        """
+
+        distances = np.hstack((dists_prediction_target, dists_target_prediction))
+        hausdorff_distance = torch.as_tensor(
+            np.percentile(
+                distances,
+                q=percentile,
+            )
+        ).float()
+
+        return hausdorff_distance
+
+    # pylint: disable=too-many-locals
+    def test_standard_case(self):
+        """
+        Tests that the CombinedPerImageMetric class correctly computes all metrics for all confidence levels.
+        """
+
+        (
+            predicted_probabilities,
+            target,
+            cardinalities_0_2,
+            cardinalities_0_5,
+            cardinalities_0_8,
+        ) = self.example_data()
+
+        tp_0_2, fp_0_2, tn_0_2, fn_0_2 = cardinalities_0_2
+        tp_0_5, fp_0_5, tn_0_5, fn_0_5 = cardinalities_0_5
+        tp_0_8, fp_0_8, tn_0_8, fn_0_8 = cardinalities_0_8
 
         expected_dice_score_0_2 = torch.as_tensor(
             2 * tp_0_2 / (2 * tp_0_2 + fp_0_2 + fn_0_2)
@@ -147,7 +176,7 @@ class TestCombinedPerImageMetric(unittest.TestCase):
         confidence_levels = [0.2, 0.5, 0.8]
 
         metrics_per_image = CombinedPerImageMetric(
-            metrics=["dice_score", "sensitivity", "specificity", "hausdorff95"],
+            metrics=metrics,
             confidence_levels=confidence_levels,
             slices_per_image=3,
         )
@@ -225,3 +254,71 @@ class TestCombinedPerImageMetric(unittest.TestCase):
             expected_hausdorff_distance_0_8,
             msg="The Hausdorff distance is computed correctly for confidence level 0.8.",
         )
+
+    def test_metric_reset(self):
+        """
+        Tests that the metrics are reset correctly.
+        """
+
+        predicted_probabilities = torch.Tensor(
+            [
+                [
+                    [0.1, 0.1],
+                    [0.9, 0.9],
+                ],
+                [
+                    [0.9, 0.9],
+                    [0.9, 0.9],
+                ],
+            ]
+        )
+
+        target = torch.Tensor(
+            [
+                [
+                    [0, 1],
+                    [1, 0],
+                ],
+                [
+                    [1, 1],
+                    [1, 1],
+                ],
+            ]
+        )
+
+        metrics = ["dice_score", "sensitivity", "specificity", "hausdorff95"]
+        confidence_levels = [0.2, 0.5, 0.8]
+
+        metrics_per_image = CombinedPerImageMetric(
+            metrics=metrics,
+            confidence_levels=confidence_levels,
+            slices_per_image=2,
+        )
+
+        metrics_per_image.update(predicted_probabilities[0], target[0])
+        metrics_per_image.compute()
+
+        metrics_per_image.reset()
+
+        metrics_per_image.update(predicted_probabilities[1], target[1])
+        computed_metrics = metrics_per_image.compute()
+
+        for metric in metrics:
+            for confidence_level in confidence_levels:
+                metric_value = computed_metrics[f"{metric}_{confidence_level}"]
+
+                if metric in ["dice_score", "sensitivity"]:
+                    self.assertTrue(
+                        torch.equal(torch.as_tensor(1.0), metric_value),
+                        "Functional implementation correctly computes dice score when there are only TN.",
+                    )
+                if metric == "hausdorff95":
+                    self.assertTrue(
+                        torch.equal(torch.as_tensor(0.0), metric_value),
+                        "Functional implementation correctly computes dice score when there are only TN.",
+                    )
+                if metric == "specificity":
+                    self.assertTrue(
+                        torch.isnan(metric_value),
+                        "Functional implementation correctly computes dice score when there are only TN.",
+                    )
