fix several bugs in metric computation

src/functional/metrics.py
@@ -719,10 +719,10 @@ class Sensitivity(SegmentationMetric):
             reduction=reduction,
         )
         self.epsilon = epsilon
-        self.true_positives = torch.tensor(0.0)
-        self.true_positives_false_negatives = torch.tensor(0.0)
-        self.add_state("true_positives", torch.tensor(0.0))
-        self.add_state("true_positives_false_negatives", torch.tensor(0.0))
+        self.true_positives = torch.zeros(num_classes)
+        self.true_positives_false_negatives = torch.zeros(num_classes)
+        self.add_state("true_positives", torch.zeros(num_classes))
+        self.add_state("true_positives_false_negatives", torch.zeros(num_classes))
 
     # pylint: disable=arguments-differ
     def update(self, prediction: torch.Tensor, target: torch.Tensor) -> None:
@@ -805,10 +805,10 @@ class Specificity(SegmentationMetric):
             reduction=reduction,
         )
         self.epsilon = epsilon
-        self.true_negatives = torch.tensor(0.0)
-        self.true_negatives_false_positives = torch.tensor(0.0)
-        self.add_state("true_negatives", torch.tensor(0.0))
-        self.add_state("true_negatives_false_positives", torch.tensor(0.0))
+        self.true_negatives = torch.zeros(num_classes)
+        self.true_negatives_false_positives = torch.zeros(num_classes)
+        self.add_state("true_negatives", torch.zeros(num_classes))
+        self.add_state("true_negatives_false_positives", torch.zeros(num_classes))
 
     # pylint: disable=arguments-differ
     def update(self, prediction: torch.Tensor, target: torch.Tensor) -> None:
@@ -904,10 +904,10 @@ class HausdorffDistance(SegmentationMetric):
         self.percentile = percentile
         self.predictions = []
         self.targets = []
-        self.hausdorff_distance = torch.tensor(0.0)
         self.add_state("predictions", [])
         self.add_state("targets", [])
-        self.add_state("hausdorff_distance", torch.tensor(0.0))
+        self.hausdorff_distance = torch.zeros(num_classes)
+        self.add_state("hausdorff_distance", torch.zeros(num_classes))
         self.all_image_locations = None
         self.hausdorff_distance_cached = False
         self.slices_per_image = slices_per_image
@@ -925,10 +925,7 @@ class HausdorffDistance(SegmentationMetric):
         self.predictions.append(prediction)
         self.targets.append(target)
 
-        if self.multi_label:
-            added_slices = 1 if prediction.dim() == 3 else prediction.shape[1]
-        else:
-            added_slices = 1 if prediction.dim() == 2 else prediction.shape[0]
+        added_slices = 1 if prediction.dim() == 3 else prediction.shape[1]
         self.number_of_slices += added_slices
 
         if self.number_of_slices == self.slices_per_image:

src/functional/utils.py
@@ -85,13 +85,14 @@ def map_ignore_index_to_true_negatives(
 
 
 def one_hot_encode(
-    tensor: torch.Tensor, num_classes: int, ignore_index: Optional[int] = None
+    tensor: torch.Tensor, dim: int, num_classes: int, ignore_index: Optional[int] = None
 ) -> torch.Tensor:
     r"""
     Converts a label encoded tensor to a one-hot encoded tensor.
 
     Args:
         tensor (Tensor): Label encoded tensor that is to be converted to one-hot encoding.
+        dim (int): Dimensionality of the input. Either 2 or 3.
         num_classes (int): Number of classes (excluding the class labeled with :attr:`ignore_label`).
         ignore_index (int, optional): Class value for which no one-hot encoded channel should be created in the output.
 
@@ -99,28 +100,34 @@ def one_hot_encode(
         Tensor: One-hot encoded tensor.
 
     Shape:
-        - Tensor: :math:`(N, X, Y, ...)` where each element represents a class index of integer type and `N = batch
-            size`.
-        - Output: :math:`(N, C, X, Y, ...)` where each element represent a binary class label and :math:`C` is the
-            number of classes (excluding the ignored class labeled with :attr:`ignore_label`).
+        - Tensor: :math:`(N, X, Y, ...)` or :math:`(X, Y, ...)` where each element represents a class index of integer
+            type and `N = batch size`.
+        - Output: :math:`(N, C, X, Y, ...)` or :math:`(C, X, Y, ...)` where each element represent a binary class label
+            and :math:`C` is the number of classes (excluding the ignored class labeled with :attr:`ignore_label`).
     """
+
+    tensor = tensor.clone()
 
     if ignore_index is not None:
         # shift labels since `torch.nn.functional.one_hot` only accepts positive labels
         tensor[tensor == ignore_index] = -1
         tensor += 1
+        num_classes += 1
 
-    tensor_one_hot = torch.nn.functional.one_hot(tensor.long(), num_classes + 1)
+    tensor_one_hot = torch.nn.functional.one_hot(tensor.long(), num_classes).int()
 
     if ignore_index is not None:
         # drop ignored channel
         tensor_one_hot = tensor_one_hot[..., 1:]
 
-    # one_hot outputs a tensor of shape (N, X, Y, ..., C)
-    # this tensor is converted to a tensor of shape (N, C, X, Y, ...)
-    return tensor_one_hot.permute(
-        (0, tensor_one_hot.ndim - 1, *range(1, tensor_one_hot.ndim - 1))
-    )
+    # one_hot outputs a tensor of shape (N, X, Y, ..., C) or (X, Y, ..., C)
+    # this tensor is converted to a tensor of shape (N, C, X, Y, ...) or (C, X, Y, ...)
+    if tensor.dim() == dim + 1:
+        # tensor has a batch dimension
+        return torch.moveaxis(tensor_one_hot, tensor_one_hot.ndim - 1, 1)
+
+    # tensor has no batch dimension
+    return torch.moveaxis(tensor_one_hot, tensor_one_hot.ndim - 1, 0)
 
 
 def validate_metric_inputs(
@@ -204,8 +211,12 @@ def preprocess_metric_inputs(
     validate_metric_inputs(prediction, target, convert_to_one_hot)
 
     if convert_to_one_hot:
-        prediction = one_hot_encode(prediction, num_classes, ignore_index=ignore_index)
-        target = one_hot_encode(target, num_classes, ignore_index=ignore_index)
+        prediction = one_hot_encode(
+            prediction, prediction.dim(), num_classes, ignore_index=ignore_index
+        )
+        target = one_hot_encode(
+            target, target.dim(), num_classes, ignore_index=ignore_index
+        )
 
     return map_ignore_index_to_true_negatives(
         prediction, target, ignore_index=ignore_index

src/metric_tracking/combined_per_epoch_metric.py
@@ -25,6 +25,9 @@ class CombinedPerEpochMetric(torchmetrics.Metric):
         image_ids (Iterable[str]): List of the ids of all images for which the metrics are to be tracked.
         slices_per_image (Union[int, List[int]]): Number of slices per 3d image. If a single integer value is
             provided, it is assumed that all images of the dataset have the same number of slices.
+        include_background_in_aggregated_metrics (bool, optional): if `False`, class channel index 0 (background class)
+            is excluded from the calculation of aggregated metrics. This parameter is used only if `multi_label` is set
+            to `False`. Defaults to `True`.
         multi_label (bool, optional): Determines whether the data is multi-label or not (default = `False`).
         confidence_levels (Iterable[float], optional): A list of confidence levels for which the metrics are to be
             tracked separately. This parameter is used only if `multi_label` is set to `True`. Defaults to `[0.5]`.
@@ -58,12 +61,14 @@ class CombinedPerEpochMetric(torchmetrics.Metric):
         id_to_class_names: Dict[int, str],
         image_ids: Iterable[str],
         slices_per_image: Union[int, List[int]],
+        include_background_in_aggregated_metrics: bool = False,
         multi_label: bool = False,
         confidence_levels: Optional[Iterable[float]] = None,
         reduction: str = "mean",
     ):
         super().__init__()
         self.metrics = metrics
+        self.include_background_in_aggregated_metrics = include_background_in_aggregated_metrics
         self.multi_label = multi_label
         self.confidence_levels = (
             confidence_levels if confidence_levels is not None else [0.5]
@@ -170,9 +175,8 @@ class CombinedPerEpochMetric(torchmetrics.Metric):
 
         aggregated_metrics = {}
 
-        for metric_name, metric_value in per_image_metrics.items():
-            aggregated_metric_name = f"{self.reduction}_{metric_name}"
-            aggregated_metrics[aggregated_metric_name] = self._reduce_metric(
+        for metric_name in per_image_metrics.keys():
+            aggregated_metrics[metric_name] = self._reduce_metric(
                 torch.tensor(per_image_metrics[metric_name])
             )
 
@@ -180,21 +184,23 @@ class CombinedPerEpochMetric(torchmetrics.Metric):
             if self.multi_label:
                 for confidence_level in self.confidence_levels:
                     average_metric = []
-                    for class_name in self.id_to_class_names:
-                        per_class_metric = aggregated_metrics[
-                            f"{metric_name}_{class_name}_{confidence_level}"
-                        ]
-                        average_metric.append(per_class_metric)
+                    for class_id, class_name in self.id_to_class_names.items():
+                        if class_id != 0 or self.multi_label or self.include_background_in_aggregated_metrics:
+                            per_class_metric = aggregated_metrics[
+                                f"{metric_name}_{class_name}_{confidence_level}"
+                            ]
+                            average_metric.append(per_class_metric)
                     aggregated_metrics[
-                        f"{metric_name}_{self.reduction}_{confidence_level}"
+                        f"{self.reduction}_{metric_name}_{confidence_level}"
                     ] = self._reduce_metric(torch.Tensor(average_metric))
             else:
                 average_metric = []
-                for class_name in self.id_to_class_names:
-                    per_class_metric = aggregated_metrics[f"{metric_name}_{class_name}"]
-                    average_metric.append(per_class_metric)
+                for class_id, class_name in self.id_to_class_names.items():
+                    if class_id != 0 or self.multi_label or self.include_background_in_aggregated_metrics:
+                        per_class_metric = aggregated_metrics[f"{metric_name}_{class_name}"]
+                        average_metric.append(per_class_metric)
                 aggregated_metrics[
-                    f"{metric_name}_{self.reduction}"
+                    f"{self.reduction}_{metric_name}"
                 ] = self._reduce_metric(torch.Tensor(average_metric))
 
         return aggregated_metrics