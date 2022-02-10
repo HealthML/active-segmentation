e11028d25ab09d02c14133bf0c08ec6215dfb343
Josafat-Mattias Burmeister <josafat-mattias.burmeister@student.hpi.de>
2022-02-10 18:23:30 +0100

    delete pascal voc dataset

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -1,9 +1,7 @@
 """ modules to import when initializing module """
 from datasets.data_module import ActiveLearningDataModule
 from datasets.brats_data_module import BraTSDataModule
-from datasets.pascal_voc_data_module import PascalVOCDataModule
 from datasets.decathlon_data_module import DecathlonDataModule
 from datasets.doubly_shuffled_nifti_dataset import DoublyShuffledNIfTIDataset
-from datasets.pascal_voc_dataset import PascalVOCDataset
 from datasets.bcss_data_module import BCSSDataModule
 from datasets.bcss_dataset import BCSSDataset
