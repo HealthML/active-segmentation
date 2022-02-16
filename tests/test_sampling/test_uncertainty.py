b6598bf6abb00ebdd1a2f56d869af8bdcbe5e4f8
ijusttyped <marcel.fernandez@mailbox.org>
2022-02-16 10:17:52 +0100

    Added entropy calculation for uncertainty and updated tests

--- tests/test_sampling/test_uncertainty.py
+++ tests/test_sampling/test_uncertainty.py
@@ -12,11 +12,14 @@ class DataLoaderMock:
     # pylint: disable=no-self-use
     """Mocks the dataloader class"""
 
+    def __init__(self, cases):
+        self.cases = cases
+
     def unlabeled_dataloader(self):
         """Mocks the unlabeled dataloader"""
         return [
             # Dataloader return one batch of slices -> batch size 2, 2 classes (background, tumor), image size 240 x 240
-            (torch.Tensor(np.zeros((2, 2, 240, 240))), ["case_1", "case_2"])
+            (torch.Tensor(np.zeros((len(self.cases), 2, 240, 240))), self.cases)
         ]
 
     def num_classes(self):
@@ -56,6 +59,9 @@ class UncertaintyTestCase(unittest.TestCase):
                         torch.Tensor(np.ones((1, 2, 240, 240))),
                     ]
                 ),
+                ["case_1", "case_2"],
+                1,
+                "distance",
                 ["case_1"],
             ),
             (
@@ -63,18 +69,39 @@ class UncertaintyTestCase(unittest.TestCase):
                     [
                         torch.tensor(np.full((1, 2, 240, 240), 0.6)),
                         torch.tensor(np.full((1, 2, 240, 240), 0.5)),
+                        torch.tensor(np.full((1, 2, 240, 240), 0.8)),
                     ]
                 ),
-                ["case_2"],
+                ["case_1", "case_2", "case_3"],
+                2,
+                "distance",
+                ["case_2", "case_1"],
+            ),
+            (
+                torch.cat(
+                    [
+                        torch.tensor(np.full((1, 2, 240, 240), 0.4)),
+                        torch.tensor(np.full((1, 2, 240, 240), 0.5)),
+                        torch.tensor(np.full((1, 2, 240, 240), 0.7)),
+                        torch.tensor(np.full((1, 2, 240, 240), 0.6)),
+                    ]
+                ),
+                ["case_1", "case_2", "case_3", "case_4"],
+                3,
+                "entropy",
+                ["case_2", "case_1", "case_4"],
             ),
         ]
     )
-    def test_select_items_to_label(self, prediction, expected_cases):
+    def test_select_items_to_label(
+        self, prediction, cases, num_cases, calculation_method, expected_cases
+    ):
         """Tests the select items method"""
         actual_cases = self.uncertainty_cls.select_items_to_label(
             models=ModelMock(prediction_tensor=prediction),
-            data_module=DataLoaderMock(),
-            items_to_label=1,
+            data_module=DataLoaderMock(cases=cases),
+            items_to_label=num_cases,
+            calculation_method=calculation_method,
             exclude_background=True,
         )
         self.assertListEqual(expected_cases, actual_cases)
