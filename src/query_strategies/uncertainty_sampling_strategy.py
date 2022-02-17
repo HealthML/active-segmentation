93226a508c40d670fc43793561e67d93afed099a
jonaskordt <jonas.kordt@student.hpi.de>
2022-02-17 16:23:02 +0100

    Extract uncertainty functions

--- src/query_strategies/uncertainty_sampling_strategy.py
+++ src/query_strategies/uncertainty_sampling_strategy.py
@@ -1,11 +1,11 @@
 """ Module for uncertainty sampling strategy """
-from typing import List, Union, Callable, Tuple, Literal
+from typing import List, Union, Tuple
 
-import numpy as np
 import torch
 
 from datasets import ActiveLearningDataModule
 from models.pytorch_model import PytorchModel
+from .functions import clean_duplicate_scans, select_uncertainty_calculation
 from .query_strategy import QueryStrategy
 
 
@@ -73,7 +73,7 @@ class UncertaintySamplingStrategy(QueryStrategy):
 
         for images, case_ids in data_module.unlabeled_dataloader():
             predictions = models.predict(images.to(device))
-            uncertainty_calculation = self.__select_uncertainty_calculation(
+            uncertainty_calculation = select_uncertainty_calculation(
                 calculation_method=self.kwargs.get("calculation_method", None)
             )
             uncertainty = uncertainty_calculation(
@@ -87,123 +87,9 @@ class UncertaintySamplingStrategy(QueryStrategy):
 
         uncertainties.sort(key=lambda y: y[0])
         if self.kwargs.get("prefer_unique_scans", False):
-            uncertainties = UncertaintySamplingStrategy.__clean_duplicate_scans(
+            uncertainties = clean_duplicate_scans(
                 uncertainties=uncertainties, items_to_label=items_to_label
             )
 
         selected_ids = [id for (_, id) in uncertainties[0:items_to_label]]
         return selected_ids, None
-
-    @staticmethod
-    def __select_uncertainty_calculation(
-        calculation_method: Literal["distance", "entropy"]
-    ) -> Callable:
-        """
-        Selects the calculation function based on the provided name.
-        Args:
-            calculation_method (str): Name of the calculation method.
-                values: `"distance"` |  `"entropy"`.
-
-        Returns:
-            A callable function to calculate uncertainty based on predictions.
-        """
-        if calculation_method == "distance":
-            return UncertaintySamplingStrategy.__distance_to_max_uncertainty
-        if calculation_method == "entropy":
-            return UncertaintySamplingStrategy.__entropy
-        print(
-            "No valid calculation method provided, choosing default method: distance to max uncertainty."
-        )
-        return UncertaintySamplingStrategy.__distance_to_max_uncertainty
-
-    @staticmethod
-    def __distance_to_max_uncertainty(
-        predictions: torch.Tensor, max_uncertainty_value: float = 0.5, **kwargs
-    ) -> np.ndarray:
-        r"""
-        Calculates the uncertainties based on the distance to a maximum uncertainty value:
-            .. math::
-                \sum | max\_uncertainty\_value - predictions |
-        Args:
-            predictions (torch.Tensor): The predictions of the model.
-            max_uncertainty_value (float, optional): The maximum value of uncertainty in the predictions.
-                (default = 0.5)
-            **kwargs: Keyword arguments specific for this calculation.
-
-        Returns:
-            Uncertainty value for each image in the batch of predictions.
-        """
-        if kwargs.get("exclude_background", False):
-            predictions = predictions[:, 1:, :, :]
-        uncertainty = (
-            torch.sum(torch.abs(max_uncertainty_value - predictions), (1, 2, 3))
-            .cpu()
-            .numpy()
-        )
-        return uncertainty
-
-    @staticmethod
-    def __entropy(
-        predictions: torch.Tensor, max_uncertainty_value: float = 0.5, **kwargs
-    ) -> np.ndarray:
-        r"""
-        Calculates the uncertainties based on the entropy of the distance to a maximum uncertainty value:
-            .. math::
-                - \sum | max\_uncertainty\_value - predictions | \cdot | \log({max\_uncertainty\_value - predictions}) |
-        Args:
-            predictions (torch.Tensor): The predictions of the model.
-            max_uncertainty_value (float, optional): The maximum value of uncertainty in the predictions.
-             (default = 0.5)
-            **kwargs: Keyword arguments specific for this calculation.
-                epsilon (float): The smoothing value to avoid the magic number.
-                    (default = 1e-10)
-
-        Returns:
-            Uncertainty value for each image in the batch of predictions.
-        """
-        # pylint: disable=unused-argument
-        # Smoothing to avoid taking log of zero
-        epsilon = kwargs.get("epsilon", 1e-10)
-        predictions[predictions == max_uncertainty_value] = (
-            max_uncertainty_value + epsilon
-        )
-        uncertainty = (
-            -torch.sum(
-                torch.multiply(
-                    torch.abs(max_uncertainty_value - predictions),
-                    torch.log(torch.abs(max_uncertainty_value - predictions)),
-                ),
-                (1, 2, 3),
-            )
-            .cpu()
-            .numpy()
-        )
-        return uncertainty
-
-    @staticmethod
-    def __clean_duplicate_scans(
-        uncertainties: List[Tuple[float, str]], items_to_label: int
-    ) -> List[Tuple[float, str]]:
-        """
-        Cleans the list from duplicate scans if possible. If minimum number of samples can't be reached without
-        duplicates, duplicates are kept.
-        Args:
-            uncertainties (List[Tuple[float, str]]): List with tuples of uncertainty value and case id.
-            items_to_label (int): Number of items that should be selected for labeling.
-
-        Returns:
-            A cleaned list of tuples.
-        """
-        cleaned_uncertainties, scan_ids, duplicate_slides = [], [], []
-        for value, case_id in uncertainties:
-            if case_id.split("-")[0] not in scan_ids:
-                scan_ids.append(case_id.split("-")[0])
-                cleaned_uncertainties.append((value, case_id))
-            else:
-                duplicate_slides.append((value, case_id))
-        if len(cleaned_uncertainties) < items_to_label:
-            return [
-                *cleaned_uncertainties,
-                *duplicate_slides[: (items_to_label - len(uncertainties))],
-            ]
-        return cleaned_uncertainties
